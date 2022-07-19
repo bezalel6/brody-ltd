91b992c5243ebc1f3efe092f30b866ec98b6c7ad
bezalel6 <bezalel3250@gmail.com>
2022-07-20 02:39:44 +0300

    vids

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
-
+import { useEffect } from "react";
+const numVids = 2;
 export default function Home() {
-
+  useEffect(()=>{
+    const vid = document.getElementById("vid");
+    let randomNum = Math.floor(Math.random() * numVids) + 1;
+    vid.src = `./vid${randomNum}.mp4`;
+      localStorage.setItem("lastRandomNum", randomNum);
+  },[]);
   return (
     <div className="container">
       <main>
-        <video loop controls id="vid" src="./vid.mp4"></video>
+        <video loop controls id="vid"></video>
+        <h3 style={{cursor:"pointer"}} onClick={()=>{
+          location.reload();
+        }}>reload the page to watch another video. we can only hope that our ginger overlord blesses us with even more videos in the future. <p>there are currently {numVids} videos in the archive</p></h3>
       </main>
     </div>
   );

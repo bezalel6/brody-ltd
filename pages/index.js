dacf690be77577df26b895fb858513f51cde5a87
bezalel6 <bezalel3250@gmail.com>
2022-07-20 02:03:28 +0300

    yup

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,12 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import { useEffect } from "react";
 
 export default function Home() {
+
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <video controls id="vid" src="./vid.mp4"></video>
       </main>
-
-      <Footer />
     </div>
-  )
+  );
 }
